7ed82b27e002f4f10bce3c3b208f9ea8c7fcb49b
chengzhycn <chengzhycn@gmail.com>
2016-04-12 16:29:57 +0800

    add udp/ltp to choose

--- ionConfGen.py
+++ ionConfGen.py
@@ -11,12 +11,17 @@
 import os
 import optparse
 import configparser
+import csv
 
-def gen_ipnadmin(rc_file, num):
+def gen_ipnadmin(rc_file, num, sort_config, protocol):
     with open(rc_file, 'a') as f:
         f.write('## begin ipnadmin\n')
-        for i in range(1, num+1):
-            f.write('a plan %s ltp/%s\n' % (i, i))
+        if protocol == 'ltp':
+            for i in range(1, num+1):
+                f.write('a plan %s ltp/%s\n' % (i, i))
+        else:
+            for item in sort_config:
+                f.write('a paln %s udp/*,%s:6767\n' % (item[0], item[1]))
         f.write('## end ipnadmin\n')
         f.write('\n')
 
@@ -32,16 +37,21 @@ def gen_cfdpadmin(rc_file):
         f.write('## end cfdpadmin\n')
         f.write('\n')
 
-def gen_bpadmin(rc_file, num, host):
+def gen_bpadmin(rc_file, num, host, protocol):
     with open(rc_file, 'a') as f:
         f.write('## begin bpadmin' + '\n' + '1\n')
         f.write("a scheme ipn 'ipnfw' 'ipnadminep'\n")
         f.write('a endpoint ipn:%s.1 q\n' % host)
         f.write('a endpoint ipn:%s.2 q\n' % host)
-        f.write('a protocol ltp 1400 100\n')
-        f.write('a induct ltp 1 ltpcli\n')
-        for i in range(1, num+1):
-            f.write('a outduct ltp %s ltpclo\n' % i)
+        if protocol == 'ltp':
+            f.write('a protocol ltp 1400 100\n')
+            f.write('a induct ltp 1 ltpcli\n')
+            for i in range(1, num+1):
+                f.write('a outduct ltp %s ltpclo\n' % i)
+        else:
+            f.write('a protocol udp 1400 100\n')
+            f.write('a induct udp 0.0.0.0:6767 udpcli\n')
+            f.write('a outduct udp * udpclo\n')
         f.write('s' + '\n' + '## end bpadmin\n')
         f.write('\n')
 
@@ -55,27 +65,30 @@ def gen_ltpadmin(rc_file, num, host, sort_config):
         f.write("## end ltpadmin\n")
         f.write("\n")
 
-def gen_ionadmin(rc_file, num, host, matrix):
+def gen_ionadmin(rc_file, num, host, matrix, protocol):
     with open(rc_file, 'a') as f:
         f.write('## begin ionadmin\n')
-        f.write("1 %s '/root/test/dtn.ionconfig'\n" % host)
+        if protocol == 'ltp':
+            f.write("1 %s '/root/test/dtn.ionconfig'\n" % host)
+        else:
+            f.write("1 %s\n" % host)
         f.write('s\n')
         f.write('m horizon +0\n')
         for i in range(1, num+1):
-            f.write('a contact +1 + 86400 %s %s 1000000\n' % (i, i))
+            f.write('a contact +1 +86400 %s %s 1000000\n' % (i, i))
         f.write('\n')
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
-                if matrix[i][j]:
+                if matrix[i][j] >= 0:
                     f.write('a contact +1 +86400 %d %d 1000000\n' % (i+1, j+1))
 
         f.write('\n')
         for i in range(1, num+1):
-            f.write('a range +1 + 86400 %s %s 0\n' % (i, i))
+            f.write('a range +1 +86400 %s %s 0\n' % (i, i))
         f.write('\n')
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
-                if matrix[i][j] and i < j:
+                if matrix[i][j] >= 0 and i < j:
                     f.write('a range +1 +86400 %d %d %d\n' % (i+1, j+1, matrix[i][j]))
 
         f.write('\n')
@@ -87,10 +100,9 @@ def gen_ionadmin(rc_file, num, host, matrix):
 def gen_file(rc_file, sort_config):
     if os.path.exists(rc_file):
         print ("Warning: %s has existed and will be rewritten!!!" % rc_file)
-        print ("To be continue?(y/n)")
-        opt = input()
-        if opt == n:
-            os._exit()
+        opt = raw_input("To be continue?(y/n)")
+        if opt == 'n':
+            os._exit(0)
 
     with open(rc_file, 'w') as f:
         f.write('# ION CONFIGURATION\n')
@@ -106,37 +118,59 @@ def load_config(config, parser):
 def sort_items(config):
     return sorted(config.items(), key = lambda d:d[0], reverse = False)
 
-def matrix_process(matrix):
+def matrix_process(matrix_path):
+    """
     matrix = matrix.strip().split(',')
     array = []
     for item in matrix:
         item = item.strip().split(' ')
         array.append(map(int, item))
     return array
+    """
+    try:
+        with open(matrix_path, 'rt') as fin:
+            cin = csv.reader(fin)
+            matrix = [map(int, row) for row in cin]
+    except Exception as e:
+        print ("ERROR: cannot locate matrix file: %s" % e)
+    return matrix
 
 if __name__ == '__main__':
     parser = optparse.OptionParser()
     parser.add_option("-c", "--config", dest="config",
                     help="config files")
+    parser.add_option("-m", "--matrix", dest="matrix",
+                    help="connection matrix, with file format in csv")
+    parser.add_option("-p", "--protocol", dest="protocol",
+                    help="udp/ltp")
     (options, args) = parser.parse_args()
 
     config_path = options.config
+    matrix_path = options.matrix
+    protocol = options.protocol
 
     read_config = configparser.ConfigParser(delimiters='=')
     read_config.read(config_path)
     config = {}
     config = load_config(config, read_config)
-
-    matrix = matrix_process(config['matrix'])
-    del config['matrix']
     num = len(config)
     sort_config = sort_items(config)
 
-    for i in range(1, num+1):
-        filename = 'host%s.rc' % i
-        gen_file(filename, sort_config)
-        gen_ionadmin(filename, num, i, matrix) 
-        gen_ltpadmin(filename, num, i, sort_config)
-        gen_bpadmin(filename, num, i)
-        gen_cfdpadmin(filename)
-        gen_ipnadmin(filename, num)
+    matrix = matrix_process(matrix_path)
+
+    if protocol == 'ltp':
+        for i in range(1, num+1):
+            filename = 'host%s.rc' % i
+            gen_file(filename, sort_config)
+            gen_ionadmin(filename, num, i, matrix, protocol) 
+            gen_ltpadmin(filename, num, i, sort_config)
+            gen_bpadmin(filename, num, i, protocol)
+            gen_cfdpadmin(filename)
+            gen_ipnadmin(filename, num, sort_config, protocol)
+    else:
+        for i in range(1, num+1):
+            filename = 'host%s.rc' % i
+            gen_file(filename, sort_config)
+            gen_ionadmin(filename, num, i, matrix, protocol)
+            gen_bpadmin(filename, num, i, protocol)
+            gen_ipnadmin(filename, num, sort_config, protocol)
